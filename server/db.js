bde8f9e5061c6a3fe0063995394459df94199e69
yurisilva2503 <yurigsfer@gmail.com>
2024-02-10 20:57:56 -0300

    finalized

--- server/db.js
+++ server/db.js
@@ -3,6 +3,6 @@ import mysql from "mysql";
 export const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '1890D82407ee!',
-    database: 'pystock'
+    password: '',
+    database: ''
 });
